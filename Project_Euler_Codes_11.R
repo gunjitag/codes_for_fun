6b91335fd0cc02382397a13d5f8da43332a6921b
Gunjita Gupta <Gunjita@Gunjitas-MacBook-Air.local>
2020-09-18 11:45:28 +0530

    problem 11

--- Project_Euler_Codes_11.R
+++ Project_Euler_Codes_11.R
@@ -5,6 +5,104 @@
 ########################################################
 
 ####################################################################################################
+#Problem 11: Largest Product in a grid
+# n the 20??20 grid below, four numbers along a diagonal line have been marked in red.
+# 
+# 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+# 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+# 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+# 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+# 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+# 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+# 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+# 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+# 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+# 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+# 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+# 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+# 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+# 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+# 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+# 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+# 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+# 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+# 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+# 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+# 
+# The product of these numbers is 26 ?? 63 ?? 78 ?? 14 = 1788696.
+# 
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20??20 grid?
+####################################################################################################
+
+#Clear the environment 
+rm(list= ls())
+
+# Input the Matrix
+M <- matrix(c(08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08,
+              49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00,
+              81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65,
+              52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91,
+              22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80,
+              24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50,
+              32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70,
+              67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21,
+              24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72,
+              21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95,
+              78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92,
+              16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57,
+              86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58,
+              19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40,
+              04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66,
+              88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69,
+              04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36,
+              20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16,
+              20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54,
+              01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48), 
+            nrow = 20, ncol = 20, byrow = TRUE)
+
+
+#Horizontal product
+product_horizontal <- c()
+for(i in 1:20){
+  for(j in 1:17){
+    product_horizontal <- c(product_horizontal, M[i,j]*M[i,j+1]*M[i,j+2]*M[i,j+3])
+  }
+}
+max_product_horizontal <- max(product_horizontal)
+
+#Vertical product
+product_vertical <- c()
+for(j in 1:20){
+  for(i in 1:17){
+    product_vertical <- c(product_vertical, M[i,j]*M[i+1,j]*M[i+2,j]*M[i+3,j])
+  }
+}
+max_product_vertical <- max(product_vertical)
+
+
+#Forward Diagnal product
+product_diagnal <- c()
+for(j in 1:17){
+  for(i in 1:17){
+    product_diagnal <- c(product_diagnal, M[i,j]*M[i+1,j+1]*M[i+2,j+2]*M[i+3,j+3])
+  }
+}
+max_product_diagnal <- max(product_diagnal)
+
+
+#Backward Diagnal product
+product_diagnal2 <- c()
+for(i in 1:17){
+  for(j in 4:20){
+    product_diagnal2 <- c(product_diagnal2, M[i,j]*M[i+1,j-1]*M[i+2,j-2]*M[i+3,j-3])
+  }
+}
+max_product_diagnal2 <- max(product_diagnal2)
+
+max_product <- max(max_product_diagnal2, max_product_diagnal, max_product_vertical, max_product_horizontal)
+
+
+####################################################################################################
 #Problem 12: Highly Divisible Triangular Number
 # The sequence of triangle numbers is generated by adding the natural numbers. 
 #So the 7th triangle number would be 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28. The first ten terms would be:
@@ -61,7 +159,7 @@ end_time - start_time
 
 #Takes a bit of time but does the job
 
-
+#################################################################################################### 
 #Problem 17: Number Letter Count
 # If the numbers 1 to 5 are written out in words: one, two, three, four, five, 
 # then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
@@ -115,8 +213,8 @@ dow_1jan1901 <- dow_1jan1900 + (365%%7)
 
 #Create a vector that stores the day on which the 1st of each month falls 
 dow <- c(dow_1jan1901)
-#Object storing what was the day of the 1st of the month
-dow_month <- 2
+#Object storing what was the day of the 1st of the current month
+dow_month <- dow
 #Month - start with Jan
 month <- 1
 #Year - start with 1901
@@ -129,6 +227,7 @@ while(year < 2001) {
     dow_month <- dow_month + (31 %% 7)
     #Start again if the calculation goes above 7
     dow_month <- if(dow_month < 8) dow_month else dow_month - 7
+    #Can also do: (dow_month + 31) %% 7 for the above two lines
     #Append to the vector
     dow <- c(dow, dow_month) 
   }
@@ -141,7 +240,7 @@ while(year < 2001) {
   }
   
   #February in leap years has 29 days
-  else if(month == 2 & (year %% 4 == 0)){
+  else if(month == 2 & ((year %% 4 == 0 & year %% 100 != 0) | year %% 400 == 0)){
     dow_month <- dow_month + (29 %% 7)
     dow_month <- if(dow_month < 8) dow_month else dow_month - 7
     dow <- c(dow, dow_month) 
